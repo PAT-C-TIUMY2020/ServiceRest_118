736a1cb5f677a2fc335c2beaed7a4d7d8f03786c
Abdul eriawan nahar <abdul.eriawan.ft18@mail.umy.ac.id>
2020-12-21 20:34:31 +0700

    Add project files.

---
+++ ITI_UMY.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using System.ServiceModel.Web;
+using System.Data.SqlClient;
+
+namespace ServiceRest_20180140118_Abdul_Eriawan_Nahar
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface ITI_UMY
+    {
+        [OperationContract]
+        [WebGet(UriTemplate = "Mahasiswa", ResponseFormat = WebMessageFormat.Json)]
+        List<Mahasiswa> GetAllMahasiswa();
+
+        [OperationContract]
+        [WebInvoke(UriTemplate = "Mahasiswa/nim=(nim)", ResponseFormat = WebMessageFormat.Json)]
+        Mahasiswa GetMahasiswaByNIM(string nim);
+
+        [OperationContract]
+        [WebInvoke(Method = "POST", UriTemplate = "Mahasiswa", RequestFormat = WebMessageFormat.Json)]
+        string CreateMahasiswa(Mahasiswa mhs);
+    }
+
+    [DataContract]
+    public class Mahasiswa
+    {
+        private string _nama, _nim, _prodi, _angkatan;
+        [DataMember(Order = 1)]
+        public string nama
+        {
+            get { return _nama; }
+            set { _nama = value; }
+        }
+        [DataMember(Order = 2)]
+        public string nim
+        {
+            get { return _nim; }
+            set { _nim = value; }
+        }
+        [DataMember(Order = 3)]
+        public string prodi
+        {
+            get { return _prodi; }
+            set { _prodi = value; }
+        }
+        [DataMember(Order = 4)]
+        public string angkatan
+        {
+            get { return _angkatan; }
+            set { _angkatan = value; }
+        }
+    }
+}
